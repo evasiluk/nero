d654952e6a95b36b397d725452092df66cdc8223
evasiluk <evasiluk@gmail.com>
2018-09-28 12:20:17 +0300

    подправил подсчет в малой корзине - из-за округлений и т.п. иногда отличались цены в малой корзине и основной

--- local/templates/.default/components/bitrix/sale.basket.basket.line/small_basket2/result_modifier.php
+++ local/templates/.default/components/bitrix/sale.basket.basket.line/small_basket2/result_modifier.php
@@ -1,14 +1,5 @@
 <?php
 //print_pre($arResult);
-$sum = 0;
-
-if($arResult["CATEGORIES"]["READY"]) {
-    foreach($arResult["CATEGORIES"]["READY"] as $item) {
-        $sum += $item["PRICE"] * $item["QUANTITY"];
-    }
-}
-
-
 $id = 30;
 switch($_SERVER["HTTP_HOST"]) {
     case BY_HOST: $id = 30;
@@ -21,6 +12,20 @@ switch($_SERVER["HTTP_HOST"]) {
         break;
 }
 
-$sum = convert_valute($sum, $id);
-$arResult["SUM"] = $sum;
+
+$sum = 0;
+$discount_sum = 0;
+
+
+if($arResult["CATEGORIES"]["READY"]) {
+    foreach($arResult["CATEGORIES"]["READY"] as $item) {
+        $item_price = convert_valute($item["BASE_PRICE"], $id);
+        $item_price = $item_price * $item["QUANTITY"];
+        $sum += $item_price;
+        $discount_sum += convert_valute($item["DISCOUNT_PRICE"] * $item["QUANTITY"], $id);
+    }
+}
+
+
+$arResult["SUM"] = $sum - $discount_sum;
 $arResult["VALUTE_SHORT"] = get_valute_short($id);
