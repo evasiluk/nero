ed6937678c33084de8a791885b858e702246cfd4
evasiluk <evasiluk@gmail.com>
2018-10-02 17:41:37 +0300

    вдогонку к предыдущему коммиту

--- content/personal/cart/make/index.php
+++ content/personal/cart/make/index.php
@@ -1,8 +1,10 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Заказы");
-?><?$APPLICATION->IncludeComponent(
+$APPLICATION->SetTitle("Оформление заказа");
+?>
+<?$APPLICATION->IncludeComponent(
     "astronim:nero.order",
     "",
     Array()
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+);?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
